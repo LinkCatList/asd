22f80d2bd8b6910d14837a01d82a56a64cf42465
Robot634 <68266302+Robot634@users.noreply.github.com>
2021-10-12 18:13:17 +0300

    Create min_in_stack.cpp

---
+++ min_in_stack.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+#define PB push_back
+#define PoB pop_back
+#define sort(a) sort(a.begin(), a.end())
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, op, x;
+    stack<ll>s;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> op;
+        if (op == 1) {
+            cin >> x;
+
+            if (s.empty()) {
+                s.push(x);
+            }
+            else {
+                if (s.top() > x) {
+                    s.push(x);
+                }
+                else {
+                    s.push(s.top());
+                }
+
+            }
+        }
+        else if (op == 2) {
+            s.pop();
+        }
+        else if (op == 3) {
+            cout << s.top() << "\n";
+        }
+    }
+
+
+    return 0;
+}
